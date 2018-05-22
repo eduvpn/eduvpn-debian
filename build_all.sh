2f934a8492109894ff98bb7e637b83470073108d
François Kooman <fkooman@tuxed.net>
2018-05-22 19:42:07 +0200

    update packages

--- build_all.sh
+++ build_all.sh
@@ -2,7 +2,7 @@
 
 REPO_DIR=${HOME}/.aptly/public
 
-sudo apt-get install build-essential devscripts aptly
+sudo apt-get -y install build-essential devscripts aptly
 
 PACKAGE_LIST=(\
     BaconQrCode \
